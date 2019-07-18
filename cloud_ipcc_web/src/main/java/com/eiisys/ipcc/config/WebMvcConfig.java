ba9a0111d6da483ef7972bdb3ecd8d0de53ca65e
hujm <hujm@53kf.com>
2019-07-18 09:01:18 +0800

    解决跨域

--- cloud_ipcc_web/src/main/java/com/eiisys/ipcc/config/WebMvcConfig.java
+++ cloud_ipcc_web/src/main/java/com/eiisys/ipcc/config/WebMvcConfig.java
@@ -5,6 +5,9 @@ package com.eiisys.ipcc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -21,23 +24,39 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public LoginInterceptor loginInterceptor() {
         return new LoginInterceptor();
     }
-    
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("POST", "GET")
+        registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*")
                 .allowCredentials(true);
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 
-        // 前台页面url模式, 需要给哪些url添加拦截器
+        // 前台页面url模式, 无需给哪些url添加拦截器
         String[] urlPatterns = {
-                "/sip/**", // sip相关
-                "/agent/**", //华为坐席相关
-                "/voiceCall/**", //语音电话相关
+                "/api/**", // 对外开放或者回调
+                "/login", //登录
+                "/tokenLogin", //token登录
+                "/error", //错误页面
+                "/webjars/**", //restfull页面
+                "/swagger-ui.html",
+                "/swagger-resources/**",
         };
 
-        registry.addInterceptor(loginInterceptor()).addPathPatterns(urlPatterns);
+        registry.addInterceptor(loginInterceptor()).addPathPatterns("/**").excludePathPatterns(urlPatterns);
     }
 }
